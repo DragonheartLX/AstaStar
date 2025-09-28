8438117f83c60ea7f26d649b60c314acbb78af04
DragonheartLX <dragonheartlx@outlook.com>
2025-09-28 20:12:42 +0800

    basic window

--- AstaStarHub/Source/AstaStarHub.cpp
+++ AstaStarHub/Source/AstaStarHub.cpp
@@ -1,10 +1,42 @@
 #include <AstaStar.h>
+#include <GLFW/glfw3.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 
-#include "AstaStarHub.h"
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << getVersion() << std::endl;
-    std::cout << str << std::endl;
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "AstaStarHub", NULL, NULL);
+
+    bgfx::PlatformData pd;
+    pd.nwh = glfwGetWin32Window(window);
+
+    bgfx::Init bgfxInit;
+    bgfxInit.platformData      = pd;
+    bgfxInit.type              = bgfx::RendererType::Count;
+    bgfxInit.resolution.width  = 1920;
+    bgfxInit.resolution.height = 1080;
+    bgfxInit.resolution.reset  = BGFX_RESET_VSYNC;
+
+    bgfx::init(bgfxInit);
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, 1920, 1080);
+
+    unsigned int counter = 0;
+    while (!glfwWindowShouldClose(window))
+    {
+        bgfx::touch(0);
+        bgfx::frame();
+        counter++;
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
